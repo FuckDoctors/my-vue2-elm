33c4e48bbb7ef5f05a025ad4feeb764cea0ceca6
FuckDoctors <zhbchwin@163.com>
2017-07-11 13:13:51 +0800

    禁用VS Code默认的CSS验证，使用StyleLint。

---
+++ .stylelintrc.js
@@ -0,0 +1,7 @@
+// https://github.com/stylelint/stylelint/blob/master/docs/user-guide/configuration.md#configuration
+module.exports = {
+  // "extends": "stylelint-config-standard",
+  "rules": {
+    // The rules property is an object whose keys are rule names and values are rule configurations.
+  }
+}
